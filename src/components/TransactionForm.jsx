0422b3ccba3b5967e8df54a8a3ec9c3e0a1038bc
Pavan Sai <pk4981@srmist.edu.in>
2024-08-07 15:04:33 +0530

    Updated placeholder value

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -5,7 +5,7 @@ const TransactionForm = () => {
   return (
     <div className='transaction-form-container'>
       <p style={{width:'160px'}}>Enter transaction UUID: </p>
-      <input id='transaction-uuid-search-box' placeholder='Transaction UUID' type="text" name="transactionUuidSearchBox" />
+      <input id='transaction-uuid-search-box' placeholder='Transaction UUID...' type="text" name="transactionUuidSearchBox" />
       <button className='search-button'>Search</button>
     </div>
   )
